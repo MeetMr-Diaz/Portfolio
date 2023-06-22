f6a74e076075250fa08e85c1ced0673649a01c9d
Max <mfdc37@umkc.edu>
2023-06-22 13:38:43 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    const sections = document.getElementsByClassName('section');
+
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].style.display = 'none';
+    }
+
+    document.getElementById(sectionId).style.display = 'block';
+  }
+
+  
